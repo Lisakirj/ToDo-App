3986f7ea554209cb1c56563cc106f4c0783d6100
Vasilisa Kirjanova <lisakirjanova097@gmail.com>
2023-10-11 20:08:42 +0300

    update classes

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -28,8 +28,8 @@ const Main = () => {
   }, [filterOpt, todoData]);
 
   return (
-    <div className="container-fluid bg-secondary p-5 vh-100">
-      <div className="container bg-white rounded-3 p-5 d-flex flex-column align-items-center">
+    <div className="container-fluid bg-secondary p-5 ">
+      <div className="container bg-white rounded-3 p-5 mb-5 d-flex flex-column align-items-center ">
         <div className="row">
           <h2>Todo List</h2>
         </div>
